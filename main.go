d4a6453f27a66541cb47efc3597153c2a3f935b5
Badal <badalWork1@gmail.com>
2023-12-17 12:08:59 +0530

    refactor: Remove unused websocker.Upgrader variable
    - The 'upgrader' variable was declared but not used in the code.
    - This commit removes the unused variable to improve code cleanliness and satisfy the linter.

--- main.go
+++ main.go
@@ -3,18 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/gorilla/websocket"
-
 	"HeyBadAl/ChatServer/handlers"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
 
 func main() {
 	http.HandleFunc("/webhook", handlers.WebhookHandler)
