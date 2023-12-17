d732cce6ea7a21cd2a8212bdc6c42e058e53ed61
Badal <badalWork1@gmail.com>
2023-12-17 07:42:21 +0530

    feat: Implement UpgradeToWebSocket function
    - This commit introduces the UpgradeToWebSocket function in utils/websocket.go.
    - The function is responsible for upgrading HTTP connections to WebSocket connections
    - It enhances the utility functions for handling WebSocket connections in the server.

---
+++ utils/websocket.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func UpgradeToWebSocket(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
